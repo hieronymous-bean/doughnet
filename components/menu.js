6ffc839e888e1b3fcc03418420eff3edea5b41cd
hieronymous-bean <jeffrey@jeffreyallanbrown.com>
2021-01-22 10:03:31 -0500

    Finish config of main nav sidebar

--- components/menu.js
+++ components/menu.js
@@ -9,7 +9,7 @@ export const menuItems = [
         id: 2,
         label: "menuitems.overview.list.dashboard.text",
         icon: "ri-dashboard-line",
-        isMenuCollapsed: true,
+        isMenuCollapsed: false,
         subItems: [
             {
                 id: 2.1,
@@ -21,8 +21,8 @@ export const menuItems = [
     {
         id: 3,
         label: "menuitems.overview.list.accounts.text",
-        icon: "ri-message-2-line",
-        isMenuCollapsed: true,
+        icon: "ri-hand-coin-line",
+        isMenuCollapsed: false,
         subItems: [
             {
                 id: 3.1,
@@ -39,13 +39,13 @@ export const menuItems = [
     {
         id: 4,
         label: "menuitems.overview.list.networth",
-        icon: "ri-message-2-line",
+        icon: "ri-wallet-line",
         link: ''
     },
     {
         id: 5,
         label: "menuitems.overview.list.transactions",
-        icon: "ri-dashboard-line",
+        icon: "ri-typhoon-line",
         link: ''
     },
 
@@ -58,19 +58,19 @@ export const menuItems = [
     {
         id: 7,
         label: "menuitems.banking.list.cashflow",
-        icon: "ri-dashboard-line",
+        icon: "ri-funds-line",
         link: ''
     },
     {
         id: 8,
         label: "menuitems.banking.list.budgeting",
-        icon: "ri-dashboard-line",
+        icon: "ri-price-tag-2-line",
         link: ''
     },
     {
         id: 9,
         label: "menuitems.banking.list.bills",
-        icon: "ri-dashboard-line",
+        icon: "ri-exchange-funds-line",
         link: ''
     },
 
@@ -81,14 +81,62 @@ export const menuItems = [
     },
     {
         id: 11,
+        label: "menuitems.planning.list.savings",
+        icon: "ri-safe-2-line",
+        link: ''
+    },
+    {
+        id: 12,
+        label: "menuitems.planning.list.retirement",
+        icon: "ri-hand-heart-line",
+        link: ''
+    },
+    {
+        id: 11,
         label: "menuitems.tools.text",
         isTitle: true
     },
     {
         id: 12,
+        label: "menuitems.tools.list.snowball",
+        icon: "ri-snowy-line",
+        link: ''
+    },
+    {
+        id: 12,
+        label: "menuitems.tools.list.avalanche",
+        icon: "ri-gps-fill",
+        link: ''
+    },
+    {
+        id: 12,
         label: "menuitems.investing.text",
         isTitle: true
-    }
+    },
+    {
+        id: 12,
+        label: "menuitems.investing.list.holdings",
+        icon: "ri-stock-fill",
+        link: ''
+    },
+    {
+        id: 12,
+        label: "menuitems.investing.list.balances",
+        icon: "ri-safe-fill",
+        link: ''
+    },
+    {
+        id: 12,
+        label: "menuitems.investing.list.performance",
+        icon: "ri-funds-box-fill",
+        link: ''
+    },
+    {
+        id: 12,
+        label: "menuitems.investing.list.allocation",
+        icon: "ri-percent-line",
+        link: ''
+    },
 
 ];
 
--- pages/dashboard/default/index.vue
+++ pages/dashboard/default/index.vue
@@ -21,100 +21,20 @@ export default {
                     active: true
                 }
             ],
-            tableTitle: "Recent Transactions",
-            productData: [{
-                    name: "ASOS Ridley High Waist",
-                    price: "$79.49",
-                    quantity: 82,
-                    amount: "$6,518.18",
-                    date: "Sep 1, 2018",
-                    sales: 54,
-                    productid: 200125
-                },
-                {
-                    name: "Marco Lightweight Shirt",
-                    price: "$128.50",
-                    quantity: 37,
-                    amount: "$4,754.50",
-                    date: "Sep 15, 2018",
-                    sales: 28,
-                    productid: 200130
-                },
-                {
-                    name: "Half Sleeve Shirt",
-                    price: "$39.99",
-                    quantity: 64,
-                    amount: "$2,559.36",
-                    date: "Nov 1, 2018",
-                    sales: 55,
-                    productid: 200140
-                },
-                {
-                    name: "Lightweight Jacket",
-                    price: "$20.00",
-                    quantity: 184,
-                    amount: "$3,680.00",
-                    date: "Nov 15, 2018",
-                    sales: 85,
-                    productid: 200250
-                },
-                {
-                    name: "Marco Shoes",
-                    price: "$28.49",
-                    quantity: 69,
-                    amount: "$1,965.81",
-                    date: "Jan 1, 2019",
-                    sales: 49,
-                    productid: 200256
-                },
-                {
-                    name: "ASOS Ridley High Waist",
-                    price: "$79.49",
-                    quantity: 82,
-                    amount: "$6,518.18",
-                    date: "Sep 1, 2018",
-                    sales: 54,
-                    productid: 200125
-                },
-                {
-                    name: "Half Sleeve Shirt",
-                    price: "$39.99",
-                    quantity: 64,
-                    amount: "$2,559.36",
-                    date: "Nov 1, 2018",
-                    sales: 55,
-                    productid: 200140
-                },
-                {
-                    name: "Lightweight Jacket",
-                    price: "$20.00",
-                    quantity: 184,
-                    amount: "$3,680.00",
-                    date: "Nov 15, 2018",
-                    sales: 85,
-                    productid: 200250
-                }
-            ],
+            transactionTableTitle: "Recent Transactions",
+            transactionData: [{
+                transactionDate: "1/1/2021",
+                transactionAccount: "Primary Account",
+                transactionDescription: "Description of this transaction",
+                transactionCategory: "Mortgage Payment",
+                transactionTags:["Tag 1","Tag 2"],
+                transactionAmount: "$415.55"
+            }],
             widgetData: [{
-                    number: "268",
-                    text: "New Customers",
-                    chartColor: "#1abc9c"
-                },
-                {
-                    number: "8574",
-                    text: "Online Orders",
-                    chartColor: "#3bafda"
-                },
-                {
-                    number: "$958.25",
-                    text: "Revenue",
-                    chartColor: "#f672a7"
-                },
-                {
-                    number: "$89.25",
-                    text: "Daily Average",
-                    chartColor: "#6c757d"
-                }
+                number: "268",
+                text: "New Customers",
+                chartColor: "#1abc9c"
+            }
             ]
         };
     }
@@ -128,30 +48,28 @@ export default {
     <div class="row">
 
         <div class="col-xl-12">
-            <Portlet :headertitle="tableTitle">
+            <Portlet :headertitle="transactionTableTitle">
                 <div class="card-body pt-0">
                     <div class="table-responsive mb-0">
                         <table class="table table-hover table-centered mb-0">
                             <thead>
                                 <tr>
-                                    <th>Transaction Id</th>
-                                    <th>Product Name</th>
-                                    <th>Price</th>
-                                    <th>Quantity</th>
-                                    <th>Amount</th>
                                     <th>Date</th>
-                                    <th>Sales</th>
+                                    <th>Account</th>
+                                    <th>Description</th>
+                                    <th>Category</th>
+                                    <th>Tags</th>
+                                    <th>Ammount</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr v-for="sellingData in productData" :key="sellingData.id">
-                                    <td>{{ sellingData.productid }}</td>
-                                    <td>{{ sellingData.name }}</td>
-                                    <td>{{ sellingData.price }}</td>
-                                    <td>{{ sellingData.quantity }}</td>
-                                    <td>{{ sellingData.amount }}</td>
-                                    <td>{{ sellingData.date }}</td>
-                                    <td>{{ sellingData.sales }}</td>
+                                <tr v-for="transactions in transactionData" :key="transactionData.id">
+                                    <td>{{ transactions.transactionDate }}</td>
+                                    <td>{{ transactions.transactionAccount }}</td>
+                                    <td>{{ transactions.transactionDescription }}</td>
+                                    <td>{{ transactions.transactionCategory }}</td>
+                                    <td>{{ transactions.transactionTags[0] }}</td>
+                                    <td>{{ transactions.transactionAmount }}</td>
                                 </tr>
                             </tbody>
                         </table>
